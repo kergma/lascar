6b1b6c4034e6b6c73b4da8397ed1a5ab77973bbe
Sergey Pushkin <pushkinsv@gmail.com>
2015-12-04 01:48:59 +0700

    detect program ip address

--- test.c
+++ test.c
@@ -32,7 +32,7 @@ struct announce_reply
 	unsigned char device_mac[6];
 	unsigned char pong7[7];
 	unsigned char unk7[7];
-	uint32_t ip;
+	struct in_addr ip;
 	unsigned char zero;
 } __attribute__((__packed__));
 
@@ -41,16 +41,13 @@ void die(char *s)
 	perror(s);
 	exit(1);
 }
-void *get_in_addr(struct sockaddr *sa)
+
+char *format_address(struct sockaddr *a)
 {
-	if (sa->sa_family == AF_INET)
-	{
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	};
-
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+	static char addrs[INET6_ADDRSTRLEN];
+	inet_ntop(a->sa_family,a->sa_family==AF_INET?(void*)&(((struct sockaddr_in*)a)->sin_addr):(void*)&(((struct sockaddr_in6*)a)->sin6_addr),addrs,sizeof addrs);
+	return addrs;
 }
-
 
 int create_listener(int socktype, const char *service)
 {
@@ -66,6 +63,8 @@ int create_listener(int socktype, const char *service)
 		die("getaddrinfo");
 	};
 	if ((s=socket(listener->ai_family, listener->ai_socktype, listener->ai_protocol)) == -1) die("socket");
+	int yes=1;
+	if (setsockopt(s,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof yes) == -1) die("setsockopt");
 	if (bind(s, listener->ai_addr, listener->ai_addrlen ) == -1) die("bind");
 	if (listener->ai_socktype==SOCK_STREAM)
 		if (listen(s, 10) == -1) die("listen");
@@ -77,12 +76,13 @@ int main (int argc, char *argv[])
 {
 	struct sockaddr_storage remoteaddr;
 	socklen_t addrlen= sizeof remoteaddr;
-	char remoteIP[INET6_ADDRSTRLEN];
 
 	int s8010, s10080, received, sent;
 	char buf[BUFLEN];
 
 	s8010=create_listener(SOCK_DGRAM,PORT8010);
+	int yes=1;
+	if (setsockopt(s8010,IPPROTO_IP,IP_PKTINFO,&yes,sizeof yes)==-1) die("setsockopt");
 
 	s10080=create_listener(SOCK_STREAM,PORT10080);
 
@@ -99,43 +99,53 @@ int main (int argc, char *argv[])
 		printf("listening on ports %s, %s ...",PORT8010,PORT10080);
 		fflush(stdout);
 		read_fds=master_fds;
-		if (select(fdmax+1,&read_fds,NULL,NULL,NULL)==-1)
-		{
-			die("select");
-			exit(1);
-		};
+		if (select(fdmax+1,&read_fds,NULL,NULL,NULL)==-1) die("select");
 		printf("\n");
 		for (int i=0;i<=fdmax;i++)
 		{
 			if (!FD_ISSET(i,&read_fds)) continue;
 			if (i==s8010)
 			{
-				if ((received = recvfrom(i, buf, BUFLEN, 0, (struct sockaddr *) &remoteaddr, &addrlen)) == -1)
+				struct iovec v[]={{buf,BUFLEN}};
+				#define CONTROLLEN 40
+				char controlbuf[CONTROLLEN];
+				struct msghdr mh={&remoteaddr,sizeof remoteaddr, v,1,controlbuf,CONTROLLEN,0};
+
+				if ((received = recvmsg(i, &mh,0))==-1) die("recvmsg");
+
+				printf("received UDP packet from %s:%d\n",format_address((struct sockaddr*)&remoteaddr),ntohs(((struct sockaddr_in*)&remoteaddr)->sin_port));
+				printf("data: %d bytes\n" , received);
+
+				struct cmsghdr *cmsg;
+				struct in_pktinfo *pi=NULL;
+				for (cmsg=CMSG_FIRSTHDR(&mh); cmsg!=NULL; cmsg=CMSG_NXTHDR(&mh,cmsg))
 				{
-					die("recvfrom");
+					if (cmsg->cmsg_level==IPPROTO_IP && cmsg->cmsg_type==IP_PKTINFO)
+					{
+						pi=(struct in_pktinfo*)CMSG_DATA(cmsg);
+						break;
+					};
+
+				};
+				if (!pi)
+				{
+					printf("could not get ancillary packet data\n");
 					exit(1);
 				};
 
-				printf("received UDP packet from %s:%d\n",inet_ntop(remoteaddr.ss_family, get_in_addr((struct sockaddr*)&remoteaddr), remoteIP, INET6_ADDRSTRLEN),ntohs(((struct sockaddr_in*)&remoteaddr)->sin_port));
-				printf("data: %d bytes\n" , received);
 				fflush(stdout);
-
 				if (received!=sizeof(struct device_announce)) continue;
 
 				struct device_announce a;
 				memmove(&a,buf,sizeof(a));
 
-				struct announce_reply r={{0,0},{0x20,0x01},{0,0,0,0,0,0},{0,0,0,0,0,0,0},{0x0f,0x80,0x01,0x65,0x05,0x80,0x0d},0x6a01a8c0/*ip hardcode*/,0};
+				struct announce_reply r={{0,0},{0x20,0x01},{0,0,0,0,0,0},{0,0,0,0,0,0,0},{0x0f,0x80,0x01,0x65,0x05,0x80,0x0d},pi->ipi_spec_dst,0};
 				memmove(&r.signature,&a.signature,sizeof(r.signature));
 				memmove(&r.device_mac,&a.mac,sizeof(r.device_mac));
 				memmove(&r.pong7,&a.ping7,sizeof(r.pong7));
 				r.unk7[3]=a.ping7[1];
 
-				if ((sent=sendto(s8010, &r, sizeof(r), 0, (struct sockaddr*) &remoteaddr, addrlen)) == -1)
-				{
-					die("sendto");
-					exit(1);
-				};
+				if ((sent=sendto(s8010, &r, sizeof(r), 0, (struct sockaddr*) &remoteaddr, addrlen)) == -1) die("sendto");
 				printf("reply sent: %d bytes\n" , sent);
 			}
 			else if (i==s10080)
@@ -145,7 +155,7 @@ int main (int argc, char *argv[])
 
 				FD_SET(newfd,&master_fds);
 				fdmax=MAX(fdmax,newfd);
-				printf("TCP connection from %s:%d on socket %d\n",inet_ntop(remoteaddr.ss_family, get_in_addr((struct sockaddr*)&remoteaddr), remoteIP, INET6_ADDRSTRLEN),ntohs(((struct sockaddr_in*)&remoteaddr)->sin_port),newfd);
+				printf("TCP connection from %s:%d on socket %d\n",format_address((struct sockaddr*)&remoteaddr),ntohs(((struct sockaddr_in*)&remoteaddr)->sin_port),newfd);
 			}
 			else if ((received = recv(i, buf, BUFLEN, 0)) <= 0)
 			{
